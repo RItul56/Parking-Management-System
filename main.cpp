739ad4104c879349b1d775d760a070badfb0b5ea
Ritul56 <ritulmastkar56@gmail.com>
2026-06-20 21:21:34 +0530

    Added vehicle rent calculation and improved parking management system

--- main.cpp
+++ main.cpp
@@ -65,30 +65,80 @@ public:
 
         string ownerName, licensePlate, vehicleType;
         int duration;
-        double rent;
 
         cout << "\nEnter Owner Name: ";
-        cin >> ownerName;
+        cin.ignore();
+        getline(cin, ownerName);
 
         cout << "Enter License Plate: ";
         cin >> licensePlate;
 
-        cout << "Enter Vehicle Type: ";
-        cin >> vehicleType;
+        int typeChoice;
+
+        cout << "\nSelect Vehicle Type\n";
+        cout << "1. Two Wheeler (Rs.20/hr)\n";
+        cout << "2. Auto Rickshaw (Rs.30/hr)\n";
+        cout << "3. Car / LLMV (Rs.50/hr)\n";
+        cout << "4. Passenger SUV / Pickup (Rs.100/hr)\n";
+        cout << "5. Truck / Bus / Heavy Vehicle (Rs.200/hr)\n";
+        cout << "Enter Choice: ";
+        cin >> typeChoice;
+        
+        switch(typeChoice)
+        {
+        case 1:
+            vehicleType = "Two Wheeler";
+            break;
+        
+        case 2:
+            vehicleType = "Auto Rickshaw";
+            break;
+        
+        case 3:
+            vehicleType = "Car";
+            break;
+        
+        case 4:
+            vehicleType = "SUV";
+            break;
+        
+        case 5:
+            vehicleType = "Heavy Vehicle";
+            break;
+        
+        default:
+            cout << "Invalid Choice!\n";
+            return;
+        }
 
         cout << "Enter Duration (hours): ";
         cin >> duration;
 
-        cout << "Enter Rent: ";
-        cin >> rent;
+        double rent;
+
+        if(vehicleType == "Two Wheeler")
+            rent = duration * 20;
+        
+        else if(vehicleType == "Auto Rickshaw")
+            rent = duration * 30;
+        
+        else if(vehicleType == "Car")
+            rent = duration * 50;
+        
+        else if(vehicleType == "SUV")
+            rent = duration * 100;
+        
+        else if(vehicleType == "Heavy Vehicle")
+            rent = duration * 200;
 
         Vehicle newVehicle(
-            ownerName,
+        ownerName,
             licensePlate,
             vehicleType,
             duration,
-            rent);
-
+            rent
+        );
+        
         vehicles.push_back(newVehicle);
 
         cout << "\nVehicle parked successfully.\n";
@@ -107,7 +157,7 @@ public:
         return;
     }
 
-    for (int i = 0; i < vehicles.size(); i++)
+    for (size_t i = 0; i < vehicles.size(); i++)
     {
         cout << "Displaying Vehicle " << i + 1 << endl;
         vehicles[i].display();
@@ -121,7 +171,7 @@ public:
         cout << "\nEnter Vehicle Number: ";
         cin >> vehicleNumber;
 
-        for (int i = 0; i < vehicles.size(); i++)
+        for (size_t i = 0; i < vehicles.size(); i++)
         {
             if (vehicles[i].getVehicleNumber() == vehicleNumber)
             {
